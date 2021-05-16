156ef5b7b674cd4622c00f47fb7c76585a66a45a
Babu Mohammed Izhan <babuizhan1234@gmail.com>
2021-05-16 15:44:21 +0530

    commit 3

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,4 +1,6 @@
-import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Total from "./Total";
 
 function App() {
   const course = 'Half Stack application development'
--- part1/src/Content.js
+++ part1/src/Content.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import Part from './Part';
+
 
 const Content = ({ props }) => {
     return (
